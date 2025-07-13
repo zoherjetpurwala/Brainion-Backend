a4aa7f5e5794f6c3d776f53f15a5a1bbd7e15e56
Zoher Jetpurwala <zoher.jetpurwala@gmail.com>
2025-07-13 23:17:41 +0530

    session updates

--- src/config/session.config.ts
+++ src/config/session.config.ts
@@ -5,11 +5,11 @@ const sessionConfig = session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure:
-      process.env.NODE_ENV === "production" && process.env.USE_HTTPS === "true",
+    secure: process.env.NODE_ENV === "production",
     httpOnly: true,
     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-    maxAge: 1000 * 60 * 60 * 24,
+    maxAge: 1000 * 60 * 60 * 24, // 24 hours
+    domain: process.env.NODE_ENV === "production" ? ".brainion.xyz" : undefined, // Key fix!
   },
 });
 
